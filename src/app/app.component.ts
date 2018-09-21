f248309c3ef4049cb83bb8e71054c4eafe703c9b
thirteenmd <andreacojocaru@gmail.com>
2018-09-21 15:46:50 +0300

    added some styling and made the table editable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,6 @@ import * as XLSX from 'xlsx';
 
 type AOA = any[][];
 
-/* import * as JSZip from 'jszip';
-import * as wjcXlsx from 'wijmo/wijmo.xlsx';
-wjcXlsx.useJSZip(JSZip); */
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,49 +12,45 @@ wjcXlsx.useJSZip(JSZip); */
 export class AppComponent {
   title = 'app';
 
+  data: AOA = [[], []];
+  fixedrows: AOA = [[], []];
+  values: AOA = [[], []];
 
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'ExportedFile.xlsx';
 
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
 
-
-
-  
-
-  data: AOA = [ [1, 2], [3, 4] ];
-	wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
-	fileName: string = 'SheetJS.xlsx';
-
-	onFileChange(evt: any) {
-		/* wire up file reader */
-		const target: DataTransfer = <DataTransfer>(evt.target);
-		if (target.files.length !== 1) throw new Error('Cannot use multiple files');
-		const reader: FileReader = new FileReader();
-		reader.onload = (e: any) => {
-			/* read workbook */
-			const bstr: string = e.target.result;
-			const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-
-			/* grab first sheet */
-			const wsname: string = wb.SheetNames[0];
-			const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-			/* save data */
-      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
-      console.log(this.data)
-		};
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      this.fixedrows = this.data.slice(0, 3);
+      this.values = this.data.slice(3);
+    };
     reader.readAsBinaryString(target.files[0]);
 
-	}
+  }
 
-	export(): void {
-		/* generate worksheet */
-		const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
 
-		/* generate workbook and add the worksheet */
-		const wb: XLSX.WorkBook = XLSX.utils.book_new();
-		XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
-		/* save to file */
-		XLSX.writeFile(wb, this.fileName);
-	}
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
 }
